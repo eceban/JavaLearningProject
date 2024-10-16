756c13c0899cc9d648119df1357effd266036106
eceban <jeniceban@gmail.com>
2024-10-16 20:47:04 +0300

    initial commit

---
+++ src/main/java/variablesandcomments/VariablesAndComments.java
@@ -0,0 +1,29 @@
+package variablesandcomments;
+
+public class VariablesAndComments {
+    public static void main(String[] args) {
+        // Declare section
+
+        boolean exampleOfBooleanVariable = true;
+        char exampleOfCharVariable;
+        int exampleOfIntVariable;
+        byte exampleOfByteVariable;
+        short exampleOfShortVariable;
+        long exampleOfLongVariable;
+        float exampleOfFloatVariable;
+        double exampleOfDoubleVariable;
+
+        // Intialization section
+
+        exampleOfCharVariable = 'C';
+        exampleOfIntVariable = 5646456;
+        exampleOfByteVariable = 12;
+        exampleOfShortVariable = 32000;
+        exampleOfLongVariable = 564564562;
+        exampleOfFloatVariable = 64.5f;
+        exampleOfDoubleVariable = 545.4564855445;
+
+        System.out.println(exampleOfShortVariable);
+
+    }
+}
