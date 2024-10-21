79c210ad33710c483fd294abf2525456ee0bb0e7
eceban <jeniceban@gmail.com>
2024-10-21 10:26:39 +0300

    commit task_2

---
+++ src/main/java/com/eugeniaqa/classesandobjects/Car.java
@@ -0,0 +1,24 @@
+package com.eugeniaqa.classesandobjects;
+
+public class Car {
+    String plateNumber;
+    int kmWhenEnteredTheService;
+    String color;
+    String make;
+    String model;
+
+    public Car(String modelOfTheCar, String makeOfTheCar, String colorOfTheCar) {
+        this.color = colorOfTheCar;
+        this.make = makeOfTheCar;
+        this.model = modelOfTheCar;
+    }
+
+    public Car(String make, String model, String color, String plateNumber, int km) {
+        this.make = make;
+        this.model = model;
+        this.color = color;
+        this.plateNumber = plateNumber;
+        this.kmWhenEnteredTheService = km;
+        System.out.println("The car object has been created with all the details");
+    }
+}
---
+++ src/main/java/com/eugeniaqa/classesandobjects/Garage.java
@@ -0,0 +1,6 @@
+package com.eugeniaqa.classesandobjects;
+
+public class Garage {
+    String name;
+    String address;
+}
